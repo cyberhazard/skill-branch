0e2ec9b110b8d3742079e567e6073606a6387c32
cyberhazard <cyberhazard@inbox.ru>
2016-11-10 14:53:37 +0300

    Add files via upload

---
+++ 2C.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+const app = express();
+
+const port = 8080;
+const host = 'localhost';
+var reg1 = /^(https?:)?(\/\/)?[\d\w\-\.]+\.?([\w]{2,4})?(\:[\d]+)?\/@?([\d\w\.]+)/i;
+var reg2 = /^[\w\d\.]+$/i;
+var reg3 = /^@[\w\d\.]+$/;
+
+app.use( (req, res, next)=> {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/',(req,res)=>{
+	console.log(req.query.username)
+	var str = req.query.username;
+	if(reg1.test(str)) res.end(`@${str.match(reg1)[5]}`)
+	if(reg2.test(str)) res.end(`@${str}`)
+	if(reg3.test(str)) res.end(`${str}`)
+	res.end('Invalid username')
+});
+
+app.listen(port,host,()=>{
+	console.log(`Сервер запущен по адресу: http://${host}:${port}`)
+})
